98e1ff1967f5444b3053f3a87773b248f1e24a23
urLegum <kanvaside@gmail.com>
2025-05-09 21:05:49 +0800

    hasil kerja

--- php/index.php
+++ php/index.php
@@ -1,3 +1,22 @@
 <?php
 
 echo "OK";
+
+class Mobil {
+    public $merek;
+    public $warna;
+    public $harga;
+
+    public function mobil($merek, $warna, $harga) {
+        $this->merek = $merek
+        $this->warna = $warna
+        $this->harga = $harga
+    }
+
+    public function getLabel() {
+        return "Merek {$this->merek}, Warna {$this->warna}, Harga {$this->harga}"
+    }
+}
+
+$obj = new mobil;
+echo $obj->getLabel("Avanza", "Merah", "10000")
